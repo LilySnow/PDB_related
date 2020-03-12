0951bdbeac709d1592a7a3247cf6805f986768e8
LilySnow <me.lixue@gmail.com>
2020-03-12 10:29:21 +0100

    fixed bugs in Info2Bfac.pl

--- Info2Bfac.pl
+++ Info2Bfac.pl
@@ -6,10 +6,8 @@
 #
 #  DESCRIPTION: extract information content from PSSM file and add it to pdb file.
 #        INPUT (pssm file):
-#          seqResNum, aa_1L, Last position-specific scoring matrix computed, weighted observed percentages rounded down, information per position, and relative weight of gapless real matches to pseudocounts
-#                     A   R   N   D   C   Q   E   G   H   I   L   K   M   F   P   S   T   W   Y   V   A   R   N   D   C   Q   E   G   H   I   L   K   M   F   P   S   T   W   Y   V
-#              1 D    -2  -2   1   7  -4  -1   2  -2  -2  -4  -4  -1  -4  -4  -2  -1  -2  -5  -4  -4    0   0   2  92   0   0   6   0   0   0   0   0   0   0   0   0   0   0   0   0  1.19 0.11
-#              2 I     0  -2  -1  -2  -1  -1  -2  -2  -2   1  -1  -1  -1  -2  -2   1   5  -3  -2   0    3   0   0   0   0   0   0   0   0  13   0   0   0   0   0   7  73   0   0   3  0.51 0.10
+#           pdbresi pdbresn seqresi seqresn    A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    T    W    Y    V   IC
+#                 1       M       1       L   -4   -4   -5   -6   -4   -3   -5   -6   -4   -1    1   -4   10   -3   -5   -4   -3   -4   -4   -2 2.32
 #
 #         Note: the following special case can happen (1acbE.pssm), where '-9-10' are connected
 #          300 49 W   -8 -3 -9-10 -8 -7 -8 -8 -2 -8 -7 -8 -7 -2 -9 -8 -8 13 -2 -8
@@ -98,27 +96,23 @@ our %three2one = (
     'PTR' => 'Y',
 );
 
-
-my $pdb_FL   = shift @ARGV;
-my $pssm_AFL = shift @ARGV;
-my $pssm_BFL = shift @ARGV;
-
-my $basename =basename ($pdb_FL, '.pdb');
-my $outputFL = "$basename.new.pdb";
-
-if ( !-e $pssm_BFL ) {
-    print "\nUsage: perl Info2Bfac.pl pdbFL pssm_chnA_FL pssm_chnB_FL\n\n";
+if (scalar @ARGV ne 3){
+    print "\nUsage: perl Info2Bfac.pl pdbFL pssm_chnID_FL chnID \n\n";
     exit;
 }
 
+my $pdb_FL   = shift @ARGV;
+my $pssmFL = shift @ARGV;
+my $chnID = shift @ARGV;
+
+my $basename =basename ($pdb_FL, '.pdb');
+my $outputFL = "$basename.$chnID.new.pdb";
+
 #----- read pssms
-my ( $pssm_A, $Info_A ) =
-  &readPSSMfl_2($pssm_AFL, 'A')
+my ( $pssm, $Info ) =
+  &readPSSMfl_2($pssmFL, $chnID)
   ;    # $pssm->{PRO:A:197} = (2 -1 4 ... ) ; $Info->{PRO:A:197} =  0.02
 
-my ( $pssm_B, $Info_B ) =
-  &readPSSMfl_2($pssm_BFL, 'B')
-  ;    # $pssm->{PRO:A:197} = (2 -1 4 ... ) ; $Info->{PRO:A:197} =  0.02
 
 #---- write output: pdb files with information content at the B-factor column
 
@@ -131,50 +125,32 @@ while (<PDB>) {
     if (/^ATOM\s+/) {
 
         my $aa         = substr( $_, 17, 3 );
-        my $chnID      = substr( $_, 21, 1 );
+        my $chnID_      = substr( $_, 21, 1 );
         my $atomResNum = substr( $_, 22, 4 );
         $atomResNum =~ s/\s+//g;
         my $key = "$aa:$chnID:$atomResNum";
 
-        if ($chnID ne 'A' &&  $chnID ne 'B'){
-            print "Error: pdb file has to have chain A and B.\n";
-            print "Current line: $_\n";
-            die "Chain ID read for this line: $chnID:$!";
+        if ($chnID_ ne $chnID){
+        # This chain is not what we are looking for
+            next;
         }
 
-        if ( $chnID eq 'A' ) {
+        if ( $chnID_ eq $chnID) {
 
             if ( &isLigand($aa) ) {
                 print "#warning: $aa is ligand\n";
-                $Info_A->{$key} = 0;
+                $Info->{$key} = 0;
             }
 
-            if ( !defined $Info_A->{$key} ) {
-                die("$key has no information content in $pssm_AFL:$!");
+            if ( !defined $Info->{$key} ) {
+                die("$key has no information content in $pssmFL:$!");
             }
 
-            my $n    = 6 - length( $Info_A->{$key} );
-            my $Info = ' ' x $n;
+            my $n    = 6 - length( $Info->{$key} );
+            my $Info_ = ' ' x $n;
 
-            $Info = $Info . $Info_A->{$key};
-            substr( $_, 60, 6, $Info );
-        }
-
-        if ( $chnID eq 'B' ) {
-
-            if ( &isLigand($aa) ) {
-                print "#warning: $aa is ligand\n";
-                $Info_B->{$key} = 0;
-            }
-
-            if ( !defined $Info_B->{$key} ) {
-                die("$key has no information content in $pssm_BFL:$!");
-            }
-
-            my $n    = 6 - length( $Info_B->{$key} );
-            my $Info = ' ' x $n;
-            $Info = $Info . $Info_B->{$key};
-            substr( $_, 60, 6, $Info );
+            $Info_ = $Info_ . $Info->{$key};
+            substr( $_, 60, 6, $Info_ );
         }
 
         print OUTPUT "$_\n";
@@ -189,24 +165,20 @@ close OUTPUT;
 
 print "$outputFL generated\n";
 
+
+
 #------------------------------
 sub readPSSMfl_2 {
 
+# add chain ID to the key to pssm
 
-# seqResNum, aa_1L, Last position-specific scoring matrix computed, weighted observed percentages rounded down, information per position, and relative weight of gapless real matches to pseudocounts
-#            A   R   N   D   C   Q   E   G   H   I   L   K   M   F   P   S   T   W   Y   V   A   R   N   D   C   Q   E   G   H   I   L   K   M   F   P   S   T   W   Y   V
-#     1 D    -2  -2   1   7  -4  -1   2  -2  -2  -4  -4  -1  -4  -4  -2  -1  -2  -5  -4  -4    0   0   2  92   0   0   6   0   0   0   0   0   0   0   0   0   0   0   0   0  1.19 0.11
-#     2 I     0  -2  -1  -2  -1  -1  -2  -2  -2   1  -1  -1  -1  -2  -2   1   5  -3  -2   0    3   0   0   0   0   0   0   0   0  13   0   0   0   0   0   7  73   0   0   3  0.51 0.10
-
-#Note: the following special case can happen (1acbE.pssm), where '-9-10' are connected
-# 300 49 W   -8 -3 -9-10 -8 -7 -8 -8 -2 -8 -7 -8 -7 -2 -9 -8 -8 13 -2 -8
 
     my $pssmFL = shift @_; #- 1EWY.protein1.ResNumPSSM
     my $chnID = shift @_; # A or B
 
 #    print "Read pssm FL ($pssmFL) ... \n";
 
-    my ($pssm,$Info) = &readPSSMfl($pssmFL);
+    my ($pssm,$Info) = &readPSSMfl_newformat($pssmFL);
     #-- $pssm->{PRO:197} = (2 -1 4 ... )
     #-- $Info->{PRO:197} = 0.23 #- information content
 
@@ -228,8 +200,77 @@ sub readPSSMfl_2 {
 
 }
 
+sub readPSSMfl_newformat {
 
-sub readPSSMfl {
+#           pdbresi pdbresn seqresi seqresn    A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    T    W    Y    V   IC
+#                 1       M       1       L   -4   -4   -5   -6   -4   -3   -5   -6   -4   -1    1   -4   10   -3   -5   -4   -3   -4   -4   -2 2.32
+
+#Note: the following special case can happen (1acbE.pssm), where '-9-10' are connected
+#  49 W   -8 -3 -9-10 -8 -7 -8 -8 -2 -8 -7 -8 -7 -2 -9 -8 -8 13 -2 -8
+
+    my $pssmFL = shift @_;
+    my %pssm;
+    my $Info; #information content for each position
+
+#    print "\n\nRead $pssmFL ... \n";
+    open( INPUT, "<$pssmFL" ) || die("Cannot open $pssmFL:$!");
+
+    while (<INPUT>) {
+        s/[\n\r]//mg;
+
+        if (/^\s*([\-\d]+\s+[a-zA-Z]{1}\s+\d+\s+[a-zA-Z]{1}\s+.+)/) {
+        #  1       M       1       L   -4   -4   -5   -6   -4   -3   -5   -6   -4   -1    1   -4   10   -3   -5   -4   -3   -4   -4   -2 2.32
+
+
+            my $line = $1;
+
+            if ( $line =~ /\d+\-\d+/ ) {
+                # '-9-10'
+                # insert a space in front of -
+                $line =~ s/-/ -/g;
+            }
+
+            my @a = split( /\s+/, $line );
+
+            if (scalar @a ne 25){
+                die("$pssmFL format is wrong! Check this line: \n$_:$!");
+            }
+
+            my $atomResNum = $a[0];
+            my $aa =  $a[1]; # 'C'
+            my $aa_3Lett = $one2three{$aa};
+
+            if (!defined $aa_3Lett){
+                die ("aa $aa is not a valid amino acid:$!");
+            }
+
+            @{ $pssm{"$aa_3Lett:$atomResNum"} } = @a[4 .. 23 ];
+
+            my $Info_onePosition = pop @a;
+
+            if (!defined $Info_onePosition){
+                die ("Info not read for $aa_3Lett:$atomResNum:$!");
+            }
+
+            $Info -> {"$aa_3Lett:$atomResNum"} = $Info_onePosition;
+
+        }
+
+    }
+    close INPUT;
+
+    if ( !%pssm || !defined $Info ) {
+
+        die("Nothing read from $pssmFL:$!")
+
+    }
+
+    return ( \%pssm, $Info);
+}
+
+
+
+sub readPSSMfl_oriformat {
 
 
 # seqResNum, aa_1L, Last position-specific scoring matrix computed, weighted observed percentages rounded down, information per position, and relative weight of gapless real matches to pseudocounts
